d53776ddc7de5ab11015dd99be886fa26c3456f8
lihansunbai <lihansunbai@163.com>
2017-09-27 23:27:07 +0800

    Initial push GHCNM database create sql script.

---
+++ SQL/GHCNM_CREATE_TABLE_tavg_metadata.sql
@@ -0,0 +1,18 @@
+CREATE TABLE temperature.ghcnm_tavg_metadata(
+    id BIGINT,
+    latitude DOUBLE PRECISION,
+    longitude DOUBLE PRECISION,
+    stnelev DOUBLE PRECISION,
+    name TEXT,
+    grelev INT,
+    popcls TEXT,
+    popsiz INT,
+    topo TEXT,
+    stveg TEXT,
+    stloc TEXT,
+    ocndis INT,
+    airstn TEXT,
+    towndis INT,
+    grveg TEXT,
+    popcss TEXT
+);
