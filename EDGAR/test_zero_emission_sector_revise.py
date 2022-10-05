a3c87987e5ecb595f137fe57cda0d20111c22646
lihansunbai <lihansunbai@163.com>
2022-10-05 13:53:08 +0800

    添加修正部门后再次栅格化的结果的部分。

--- EDGAR/test_zero_emission_sector_revise.py
+++ EDGAR/test_zero_emission_sector_revise.py
@@ -31,9 +31,10 @@ end_year = 2018
 working_point_feature = []
 
 # 列出要处理的点数据
-for yr in range(start_year,end_year+1):
+for yr in range(start_year, end_year+1):
     temp_wild_card = 'sectoral_weights_%s' % yr
-    working_point_feature.extend(arcpy.ListFeatureClasses(wild_card=temp_wild_card,feature_type='Point'))
+    working_point_feature.extend(arcpy.ListFeatureClasses(
+        wild_card=temp_wild_card, feature_type='Point'))
 
 # 检查列出点数据是否成狗
 if len(working_point_feature) == 0:
@@ -44,10 +45,10 @@ if len(working_point_feature) == 0:
 for pt in working_point_feature:
     print 'start revise %s' % pt
 
-    #--first lets make a list of all of the fields in the table
+    # --first lets make a list of all of the fields in the table
     fields = arcpy.ListFields(pt)
     field_names = [field.name for field in fields]
-    ## 注意：
+    # 注意：
     # 根据arcpy文档给出的说明：
     # UpdateCursor 用于建立对从要素类或表返回的记录的读写访问权限。
     # 返回一组迭代列表。 列表中值的顺序与 field_names 参数指定的字段顺序相符。
@@ -69,3 +70,28 @@ for pt in working_point_feature:
                     row[index_wraster] = 0
                     row[index_wmaxid] = 'NULL'
 
+            # 更新数据
+            cursor.updateRow(row)
+
+    save_raster_categories = 'main_emi_%s' % pt[-4:]
+    save_raster_weight = 'main_emi_weight_%s' % pt[-4:]
+    # 用wraster列转栅格
+    try:
+        arcpy.PointToRaster_conversion(pt,
+                                       'wraster',
+                                       save_raster_categories,
+                                       'MOST_FREQUENT',
+                                       '#',
+                                       '0.1')
+        arcpy.PointToRaster_conversion(pt,
+                                       'wmax',
+                                       save_raster_weight,
+                                       'MOST_FREQUENT',
+                                       '#',
+                                       '0.1')
+        print 'Create main emission raster: %s' % pt
+
+    except:
+        print 'Create main emission raster field: %s' % pt
+
+        print arcpy.GetMessages()
